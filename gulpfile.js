1201d75bba5e569aeb92b24e377ba5828828f73b
AndreeaPenu <andreea_penu@hotmail.Com>
2018-07-31 17:22:52 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+
+
+gulp.task('default', ['imageMin','minify','sass']);
+
+gulp.task('imageMin', () =>
+    gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('minify', function(){
+    gulp.src('src/js/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('sass', function(){
+    gulp.src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+})
+
+gulp.task('watch', function(){
+    gulp.watch('src/images/*', ['imageMin']);
+    gulp.watch('src/js/*.js', ['minify']);
+    gulp.watch('src/sass/*.scss', ['sass']);
+});

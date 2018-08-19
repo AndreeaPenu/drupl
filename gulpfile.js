3cc7af9459879af9dfb145e278ef21b91de764c0
AndreeaPenu <andreea_penu@hotmail.Com>
2018-08-19 12:49:05 +0200

    moving pages to root lvl

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 
 
-gulp.task('default', ['imageMin','minify','sass']);
+gulp.task('default', ['imageMin', 'minify', 'sass']);
 
 gulp.task('imageMin', () =>
     gulp.src('src/images/*')
@@ -12,19 +12,19 @@ gulp.task('imageMin', () =>
     .pipe(gulp.dest('dist/images'))
 );
 
-gulp.task('minify', function(){
+gulp.task('minify', function() {
     gulp.src('src/js/*')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 })
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
     gulp.src('src/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
 })
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
     gulp.watch('src/images/*', ['imageMin']);
     gulp.watch('src/js/*.js', ['minify']);
     gulp.watch('src/sass/*.scss', ['sass']);
